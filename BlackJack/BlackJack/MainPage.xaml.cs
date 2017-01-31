68ef78d37890a1d7c6d2bea9c404fd66c8f6f1c0
sylvainBrunet <sylvain69740@gmail.com>
2017-01-31 11:10:33 +0100

    Formulaire inscriptions

--- BlackJack/BlackJack/MainPage.xaml.cs
+++ BlackJack/BlackJack/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace BlackJack
         {
             this.InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(signup));
+        }
     }
 }
